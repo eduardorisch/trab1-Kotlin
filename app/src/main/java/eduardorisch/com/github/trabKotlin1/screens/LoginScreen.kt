25c1391e4fd2b381578507a4a4f57edafc757370
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-12 21:24:37 -0300

    first commit

---
+++ app/src/main/java/eduardorisch/com/github/trabKotlin1/screens/LoginScreen.kt
@@ -0,0 +1,46 @@
+package eduardorisch.com.github.trabKotlin1.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier){
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFED145B))
+            .padding(32.dp)
+    ){
+        Text(
+            text = "LOGIN",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Button(
+            onClick = {/*todo*/},
+            colors = ButtonDefaults.buttonColors(Color.White),
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(
+                text = "ENTRAR",
+                fontSize = 20.sp,
+                color = Color.Blue
+            )
+        }
+    }
+
+}
